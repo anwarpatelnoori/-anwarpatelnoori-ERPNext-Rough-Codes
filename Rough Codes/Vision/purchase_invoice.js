7b829e1d13afa024d0519b3d9c843b96c87ff542
anwarpatelnoori <anwarpatelrazvi@gmail.com>
2024-10-12 18:22:28 +0530

    12 Oct

--- Rough Codes/Vision/purchase_invoice.js
+++ Rough Codes/Vision/purchase_invoice.js
@@ -1,14 +1,10 @@
 frappe.ui.form.on('Purchase Invoice', {
-    before_load: function (frm) {
-        hide_fields(frm)
+    refresh: function (frm) {       
         default_warehouse(frm)
-    },
-    onload: function (frm) {
         hide_fields(frm)
-        default_warehouse(frm)
-    },
-    refresh: function (frm) {
-        hide_fields(frm)
+        if (frm.doc.__islocal){
+            frm.set_value('custom_current_employee_user_id',frappe.user.name)
+        }
     },
     before_save: function (frm) {
         default_cost_center(frm)
@@ -20,6 +16,11 @@ frappe.ui.form.on('Purchase Invoice', {
     }
 })
 function default_cost_center(frm) {
+    console.log('Default Cost Center');
+    
+    if (frm.is_new()){
+
+    }
     frm.doc.items.forEach((item, index) => {
         frappe.model.set_value(item.doctype, item.name, 'cost_center', frm.doc.custom_cost_center);
     });
@@ -36,16 +37,12 @@ function hide_fields(frm) {
         const fieldsToHide = [
             'posting_date', 'posting_time', 'set_posting_time', 'apply_tds', 'accounting_dimensions_section', 'scan_barcode',
             'currency_and_price_list', 'tax_category', 'shipping_rule', 'incoterm', 'rounding_adjustment', 'rounding_adjustment', 'total_advance', 'rejected_warehouse',
-            'is_subcontracted', 'raw_materials_supplied', 'rounded_total', 'use_company_roundoff_cost_center', 'taxes_and_charges_added', 'taxes_and_charges_deducted',
-            'update_stock'
+            'is_subcontracted', 'raw_materials_supplied', 'rounded_total', 'use_company_roundoff_cost_center', 'taxes_and_charges_added', 'taxes_and_charges_deducted'
 
         ];
         fieldsToHide.forEach(fieldname => {
             frm.set_df_property(fieldname, 'hidden', 1);
         });
-        frm.set_value('ignore_pricing_rule', 1)
-        frm.set_value('update_stock', 1)
-
         // Hide sections
         const tab_break_to_hide = ['purchase-invoice-payments_tab-tab', 'purchase-invoice-terms_tab-tab', 'purchase-invoice-address_and_contact_tab-tab',
             'purchase-invoice-terms_tab-tab', 'purchase-invoice-terms_tab-tab', 'purchase-invoice-more_info_tab-tab'
